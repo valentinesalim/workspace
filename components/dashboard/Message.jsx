3649d19042ff50c08fac7e1276e35c9e04203617
aryanvichare <aryanvichare10@gmail.com>
2021-03-06 15:31:31 -0800

    Add Chat Link Support

--- components/dashboard/Message.jsx
+++ components/dashboard/Message.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 const Message = ({ msg, sender, photoURL, time }) => {
+  const URLExpression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+
+  const regex = new RegExp(URLExpression);
+  const isLink = msg.match(regex);
+
   return (
     <div className="flex flex-row justify-start w-full overflow-x-scroll">
       <img
@@ -13,8 +18,20 @@ const Message = ({ msg, sender, photoURL, time }) => {
           <p className="text-xs mb-px">{sender} • </p>
           <span className="ml-1 text-gray-50">{time}</span>
         </div>
-
-        <p className="text-sm">{msg}</p>
+        <p className="text-sm">
+          {!isLink ? (
+            msg
+          ) : (
+            <a
+              className="text-blue-400"
+              href={msg}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {msg}
+            </a>
+          )}
+        </p>
       </div>
     </div>
   );
