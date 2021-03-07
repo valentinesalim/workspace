bf1b9794779864bff9a933b7ffaedd280281142c
aryanvichare <aryanvichare10@gmail.com>
2021-03-06 23:28:54 -0800

    emit and receive socketio messages

--- socketserver/testserver/app.js
+++ socketserver/testserver/app.js
@@ -1,32 +1,30 @@
-// Copyright 2018 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-'use strict';
-
-// [START appengine_websockets_app]
 const app = require('express')();
-app.set('view engine', 'pug');
+const cors = require('cors');
+
+app.use(cors());
 
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
-
-app.get('/', (req, res) => {
-  res.render('index.pug');
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
 });
 
-io.on('connection', socket => {
-  socket.on('chat message', msg => {
+app.get('/', (req, res) => {
+  res.send('API is running!');
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected!', socket.id);
+
+  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+
+  socket.on('clear_whiteboard', (data) =>
+    socket.broadcast.emit('clear_whiteboard')
+  );
+
+  socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
 });
@@ -38,6 +36,5 @@ if (module === require.main) {
     console.log('Press Ctrl+C to quit.');
   });
 }
-// [END appengine_websockets_app]
 
 module.exports = server;
